e3be38c0650dce4903d9e1532bbbdaa44c63b4db
Veronica Viarengo <veronica.viarengo@scaifinance.it>
2020-11-21 21:32:13 +0100

    Progress bar animation

---
+++ public/progress-bar.js
@@ -0,0 +1,20 @@
+const COUNTER_INTERVAL_TIME = 40;
+$(document).ready(function() {
+    console.log("DOCUMENT READY");
+    const progress = $('.progress-animation');
+    if(progress){
+        const currentValue = progress.data("value");
+        let counter = 0;
+        const progressBar = progress.children(".progress-bar");
+        const remaning = progress.find("h6 strong span");
+        const interval = setInterval(function(){
+            progressBar.css("width", counter+"%");
+            progressBar.text(counter + "% ");
+            remaning.text((100-counter)+ "% ");
+            if(counter===currentValue){
+                clearInterval(interval);
+            }
+            counter++;
+        },COUNTER_INTERVAL_TIME);
+    }
+});
