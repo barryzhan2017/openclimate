ff079c8466d65b14c27b11c34ca483c62fa040b1
veronicadev <viarengoveronica@gmail.com>
2020-11-23 16:13:47 +0100

    Percentage Calc

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,8 +5,15 @@ const Contact = require("./../models/contactModel");
 exports.getContactsByZipCode = async (req, res) => {
   try {
     const contact = await Contact.find({ zipCode: req.body.zipCode });
+    //percentage calc
+    let percentage = 0;
     if (contact.length > 0) {
-      res.status(200).render("base", { data: contact });
+      const targetLeft = parseInt((contact[0].countryGHGCurrent - contact[0].countryGHGTarget) / contact[0].countryGHGCurrent * 100);
+      percentage = 100 - targetLeft;
+      if(percentage>100){
+        percentage = 100;
+      }
+      res.status(200).render("base", { data: contact, percentage: percentage});
     }
     else {
       res.render('index', { error: true });
