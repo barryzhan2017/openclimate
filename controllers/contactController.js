17da704c4cabd636f33515eb0dd6f4d2dbf292ca
veronicadev <viarengoveronica@gmail.com>
2020-11-23 19:20:36 +0100

    fix percentage & pictures

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -6,13 +6,13 @@ exports.getContactsByZipCode = async (req, res) => {
   try {
     const contact = await Contact.find({ zipCode: req.body.zipCode });
     //percentage calc
-    let percentage = 0;
+    let percentage = 10;
     if (contact.length > 0) {
-      const targetLeft = parseInt((contact[0].countryGHGCurrent - contact[0].countryGHGTarget) / contact[0].countryGHGCurrent * 100);
+      /*const targetLeft = parseInt((contact[0].countryGHGCurrent - contact[0].countryGHGTarget) / contact[0].countryGHGCurrent * 100);
       percentage = 100 - targetLeft;
       if(percentage>100){
         percentage = 100;
-      }
+      }*/
       res.status(200).render("base", { data: contact, percentage: percentage});
     }
     else {
