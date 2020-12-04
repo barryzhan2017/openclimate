e812cb3b2cf2811b3bde35c4d77e2f18a1033867
veronicadev <viarengoveronica@gmail.com>
2020-12-04 12:33:11 +0100

    Menu active element

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ var router = express.Router();
 
 /* GET about. */
 router.get('/about', function (req, res, next) {
-    res.render('about', { error: false, title: "ClimateReps - About" });
+    res.render('about', { 
+        error: false, 
+        title: "ClimateReps - About",
+        path: req.path
+    });
 });
 
 /* GET about. */
 router.get('/question-answers', function (req, res, next) {
-    res.render('question-answers', { error: false, title: "ClimateReps - Question & Answers" });
+    res.render('question-answers', { 
+        error: false, 
+        title: "ClimateReps - Question & Answers",
+        path: req.path
+    });
 });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { error: false, title: "ClimateReps - The first open climate democracy website" });
+    res.render('index', {
+         error: false, 
+         title: "ClimateReps - The first open climate democracy website",
+         path: req.path
+    });
 });
 
 module.exports = router;
