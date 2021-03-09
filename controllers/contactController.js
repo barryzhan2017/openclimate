00816151c3c18d001398438dd09729ebb798b97a
StevenMcDonald97 <sam489@cornell.edu>
2021-03-08 23:11:42 -0800

    added changes to controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,23 +1,23 @@
-// const Contact = require("./../models/contactModel");
+const Contact = require("./../models/contactModel");
 
 
 
 exports.getContactsByZipCode = async (req, res) => {
   try {
-    // const contact = await Contact.find({ zipCode: req.body.zipCode });
-    const contact=[{zipCode:'94087', city:'Sunnyvale', county:'', state:'California'}]
+    const contact = await Contact.find({ zipCode: req.body.zipCode });
+    // const contact=[{zipCode:'94087', city:'Sunnyvale', county:'', state:'California'}]
     //percentage calc
     let percentageCountry = 0;
     let percentageGovernor = 0;
     let percentageMayor = 0;
 
     if (contact.length > 0) {
-      // const targetLeft = parseInt((contact[0].countryGHGCurrent - contact[0].countryGHGTarget) / contact[0].countryGHGCurrent * 100);
-      // const governorTargetLeft = parseInt((contact[0].governorGHGCurrent - contact[0].governorGHGTarget) / contact[0].governorGHGCurrent * 100);
-      // const mayorTargetLeft = parseInt((contact[0].mayorGHGCurrent - contact[0].mayorGHGTarget) / contact[0].mayorGHGCurrent * 100);
-      const targetLeft = 10;
-      const governorTargetLeft = 15;
-      const mayorTargetLeft = 20;
+      const targetLeft = parseInt((contact[0].countryGHGCurrent - contact[0].countryGHGTarget) / contact[0].countryGHGCurrent * 100);
+      const governorTargetLeft = parseInt((contact[0].governorGHGCurrent - contact[0].governorGHGTarget) / contact[0].governorGHGCurrent * 100);
+      const mayorTargetLeft = parseInt((contact[0].mayorGHGCurrent - contact[0].mayorGHGTarget) / contact[0].mayorGHGCurrent * 100);
+      // const targetLeft = 10;
+      // const governorTargetLeft = 15;
+      // const mayorTargetLeft = 20;
 
 
       percentageCountry = 100 - targetLeft;
