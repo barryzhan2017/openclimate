8ae1d9a3b724ebebf6204cfbe34ac7b5c481a3d2
Steven McDonald <sam489@cornell.edu>
2021-03-09 00:46:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,20 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//     '<PASSWORD>',
-//     process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
-// mongoose
-//     .connect(DB, {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true
-//     })
-//     .then(con => console.log('DB connection is successful.'));
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(con => console.log('DB connection is successful.'));
 
-
-mongoose.connect('mongodb://localhost/openClimate', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  });
 const port = process.env.PORT;
 
 app.listen(port, () => {
