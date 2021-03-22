0916d32935f8e71c1311d0873fd3214e69c1f3f3
StevenMcDonald97 <sam489@cornell.edu>
2021-03-22 00:31:52 -0700

    got rid of attempt to streamline fetching state data

--- controllers/myStateController.js
+++ controllers/myStateController.js
@@ -1,22 +1,43 @@
 const Contact = require("./../models/contactModel");
-const states = require("./stateData"); 
 
 exports.getStateData= async (req, res) => {
-
-  let stateData={}
+  let stateData={};
   const stateArray = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
   let count=0;
 
   try {
 
-    res.status(200).render("myState", { 
-      states: states, 
-      searchImagePath: "../../public/images/my-state-search.png"
+     stateArray.forEach((state)=>{
+      try {
+        Contact.findOne({ state: state }).exec(function(err,info){
+          if (info){
+            let governorTargetLeft = parseInt((info.governorGHGCurrent - info.governorGHGTarget) / info.governorGHGCurrent * 100);
+            stateData[state] = 100-governorTargetLeft;
+          }
+          count++;
+          if (count===stateArray.length || count>51){
+            res.status(200).render("myState", { 
+              states: stateData, 
+              searchImagePath: "../../public/images/my-state-search.png"
+            });
+          }
+        });
+      } catch (err) {
+        console.log(err);
+        count++;
+      }
+      // stateData[state]=50;
+      // count++;
+      // if (count===stateArray.length){
+      //   res.status(200).render("myState", { 
+      //     states: stateData, 
+      //     searchImagePath: "../../public/images/my-state-search.png"
+      //   });
+      // }
     });
-    // stateData['teststate']=85;
 
   } catch (err) {
-
+    console.log(err);
     res.status(404).json({
       status: "fail",
       message: err
--- routes/stateRoutes.js
+++ routes/stateRoutes.js
@@ -2,11 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 const myStateController = require("../controllers/myStateController.js");
-const updateStatesController = require("../controllers/updateStatesController.js");
 
 // router.param('id', tourController.checkID);
 
-router.get('/update/', updateStatesController.updateData);
 router.get('/', myStateController.getStateData);
 
 
